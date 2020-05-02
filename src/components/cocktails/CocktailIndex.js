8a954031b359564950416c5cef54e4c30a335ae0
abigailforeman <forea005@gmail.com>
2020-05-02 15:23:16 +0100

    updated search feature

--- src/components/cocktails/CocktailIndex.js
+++ src/components/cocktails/CocktailIndex.js
@@ -6,51 +6,117 @@ import CocktailCard from './CocktailCard'
 class CocktailIndex extends React.Component {
   state = {
     cocktails: [],
-    showCocktails: []
+    showCocktails: [],
+    spirits: [
+      'Vodka',
+      'Gin',
+      'Whisky',
+      'Rum',
+      'Tequila',
+      'Kahlua',
+      'Bourbon',
+      'Coffee Liqueur'
+    ],
+    chosenSpirit: ''
   }
 
-  async componentDidMount() {
+  // async componentDidMount () {
+  //   try {
+  //     const res = await axios.get(
+  //       'https://www.thecocktaildb.com/api/json/v1/1/search.php?s='
+  //     )
+  //     this.setState({
+  //       cocktails: res.data.drinks,
+  //       showCocktails: res.data.drinks
+  //     })
+  //     console.log(res)
+  //   } catch (err) {
+  //     console.log(err)
+  //     this.props.history.push('/error')
+  //   }
+  // }
+
+  displayCocktails = async () => {
     try {
-      const res = await axios.get('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=')
-      this.setState({ cocktails: res.data.drinks, showCocktails: res.data.drinks })
-    } catch (err) {
-      console.log(err)
-      this.props.history.push('/error')
+      const res = await axios.get(
+        `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${this.state.chosenSpirit}`
+      )
+      this.setState({ showCocktails: res.data.drinks })
+    } catch (error) {
+      console.log(error)
     }
   }
 
   handleChange = e => {
     e.preventDefault()
-    // console.log(e.target.value)
     const alcoholSelected = e.target.value
-    // console.log(alcoholSelected)
-    const filterCocktails = this.state.cocktails.filter(cocktail => (cocktail.strAlcoholic === alcoholSelected || alcoholSelected === 'All'))
+    const filterCocktails = this.state.cocktails.filter(
+      cocktail =>
+        cocktail.strAlcoholic === alcoholSelected || alcoholSelected === 'All'
+    )
     this.setState({ showCocktails: filterCocktails })
   }
 
-  render() {
-    // console.log(this.state.showCocktails)
+  handleInput = e => {
+    const chosenSpirit = e.target.value
+    this.setState({ chosenSpirit })
+  }
+
+  render () {
+    if (!this.state.showCocktails) return null
+    console.log(
+      'console logging chosen spirit from render',
+      this.state.chosenSpirit
+    )
     return (
-      <div className="index-wrapper">
-        <div className="title-wrapper">
-          <h1 className="main-title">THE COCKTAIL CLUB</h1>
+      <div className='index-wrapper'>
+        <div className='title-wrapper'>
+          <h1 className='main-title'>THE COCKTAIL CLUB</h1>
         </div>
 
-        <div className="form-wrapper">
-          <form className="alcoholic-form">
-            <select onChange={this.handleChange} name="cocktails" id="cocktails">
-              <option value="All">All</option>
-              <option value="Alcoholic">Alcoholic</option>
-              <option value="Optional alcohol">Optional Alcoholic</option>
-              <option value="Non alcoholic">Non-Alcoholic</option>
+        <div className='form-wrapper'>
+          <form className='alcoholic-form'>
+            <select
+              onChange={this.handleChange}
+              name='cocktails'
+              id='cocktails'
+              defaultValue={'DEFAULT'}
+            >
+              <option value='DEFAULT' disabled>ALCOHOL/NON-ALCOHOL</option>
+              <option value='All'>All</option>
+              <option value='Alcoholic'>Alcoholic</option>
+              <option value='Optional alcohol'>Optional Alcohol</option>
+              <option value='Non alcoholic'>Non-Alcoholic</option>
             </select>
             {/* <input type="text" name='cocktail-search' id="cocktail-search" placeholder="Search cocktails..." value="" /> */}
           </form>
+
+          <form className='spirit-form'>
+            <select
+              onChange={this.handleInput}
+              name='spirits'
+              id='spirits'
+              defaultValue={'DEFAULT'}
+            >
+              <option value='DEFAULT' disabled>
+                SELECT SPIRIT
+              </option>
+              {this.state.spirits.map(spirit => (
+                <option key={spirit} value={spirit}>
+                  {spirit}
+                </option>
+              ))}
+            </select>
+          </form>
+
+          <button className='button is-white is-medium is-rounded' id='search-button' onClick={this.displayCocktails}>
+            FIND COCKTAIL!
+          </button>
         </div>
 
-        <section className="section">
-          <div className="container">
-            <div className="columns is-mobile is-multiline">
+        <section className='section'>
+          <div className='container'>
+            <div className='columns is-mobile is-multiline'>
               {this.state.showCocktails.map(cocktail => (
                 <CocktailCard key={cocktail.idDrink} {...cocktail} />
               ))}
